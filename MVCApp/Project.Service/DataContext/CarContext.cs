60d537ae91951cc1d73ae08255250df7853f775b
One4Solution <start2play4fun@gmail.com>
2020-01-23 11:57:46 +0100

    Created models, database and tables

---
+++ MVCApp/Project.Service/DataContext/CarContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Project.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Project.Service.DataContext
+{
+    public class CarContext : DbContext
+    {
+        public CarContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<VehicleMake> VehicleMake { get; set; }
+        public DbSet<VehicleModel> VehicleModel { get; set; }
+
+
+        public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CarContext>
+        {
+            public CarContext CreateDbContext(string[] args)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile(@Directory.GetCurrentDirectory() + "/../Project.MVC/appsettings.json")
+                    .Build();
+
+                var builder = new DbContextOptionsBuilder<CarContext>();
+                var connectionString = configuration.GetConnectionString("CarDB");
+                builder.UseSqlServer(connectionString);
+                return new CarContext(builder.Options);
+                    
+            }
+        }
+
+
+    }
+}
---
+++ MVCApp/Project.Service/Models/VehicleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Service.Models
+{
+    public class VehicleModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+
+        public virtual int VehicleMakeId { get; set; }
+        public virtual VehicleMake VehicleMake { get; set; }
+    }
+}
